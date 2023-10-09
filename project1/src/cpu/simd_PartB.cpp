368c2ae1bbf41a36a92ccc134d9bd43885cc6494
Congyuwang <116020237@link.cuhk.edu.cn>
2023-10-09 05:06:11 -0400

    changed to use 256bit

--- project1/src/cpu/simd_PartB.cpp
+++ project1/src/cpu/simd_PartB.cpp
@@ -11,35 +11,26 @@ const float filter[FILTER_SIZE][FILTER_SIZE] = {
   { 1.0 / 9, 1.0 / 9, 1.0 / 9 }
 };
 
-const unsigned short LOAD_MASK = 0b111111111;
-
-inline __m512
+inline __m256
 load_row(unsigned char* base)
 {
-  auto chars = _mm_maskz_loadu_epi8(LOAD_MASK, base);
-  auto ints = _mm512_maskz_cvtepu8_epi32(LOAD_MASK, chars);
-  return _mm512_maskz_cvtepi32_ps(LOAD_MASK, ints);
+  auto chars = _mm_loadu_si128((__m128i*)base);
+  auto ints = _mm256_cvtepu8_epi32(chars);
+  return _mm256_cvtepi32_ps(ints);
 }
 
-__m512
+__m256
 row_filter(int row)
 {
-  return _mm512_setr_ps(filter[row][0], // 0, 0
+  return _mm256_setr_ps(filter[row][0], // 0, 0
                         filter[row][0], // 0, 1
                         filter[row][0], // 0, 2
                         filter[row][1], // 0, 3
                         filter[row][1], // 1, 0
                         filter[row][1], // 1, 1
                         filter[row][2], // 1, 2
-                        filter[row][2], // 1, 3
-                        filter[row][2], // 2, 0
-                        0.0,
-                        0.0,
-                        0.0,
-                        0.0,
-                        0.0,
-                        0.0,
-                        0.0);
+                        filter[row][2]  // 1, 3
+  );
 }
 
 int
@@ -61,9 +52,9 @@ main(int argc, char** argv)
   }
 
   // Prepare Consts
-  __m512 filter_t = row_filter(0);
-  __m512 filter_m = row_filter(1);
-  __m512 filter_b = row_filter(2);
+  __m256 filter_t = row_filter(0);
+  __m256 filter_m = row_filter(1);
+  __m256 filter_b = row_filter(2);
 
   // Apply the filter to the image
   auto filteredImage = new unsigned char[input_jpeg.width * input_jpeg.height *
@@ -88,24 +79,29 @@ main(int argc, char** argv)
       unsigned char* bot_base = mid_base + row_length;
 
       // load value
-      __m512 row_t = load_row(top_base);
-      __m512 row_m = load_row(mid_base);
-      __m512 row_b = load_row(bot_base);
+      __m256 row_t = load_row(top_base);
+      __m256 row_m = load_row(mid_base);
+      __m256 row_b = load_row(bot_base);
+      float cv_r_t_b = top_base[8];
+      float cv_r_m_b = mid_base[8];
+      float cv_r_b_b = bot_base[8];
 
       // apply filter
-      row_t = _mm512_maskz_mul_ps(LOAD_MASK, row_t, filter_t);
-      row_m = _mm512_maskz_mul_ps(LOAD_MASK, row_m, filter_m);
-      row_b = _mm512_maskz_mul_ps(LOAD_MASK, row_b, filter_b);
+      row_t = _mm256_mul_ps(row_t, filter_t);
+      row_m = _mm256_mul_ps(row_m, filter_m);
+      row_b = _mm256_mul_ps(row_b, filter_b);
+      cv_r_t_b *= filter[0][2];
+      cv_r_m_b *= filter[1][2];
+      cv_r_b_b *= filter[2][2];
 
       // aggregate rows
-      __m512 result = _mm512_maskz_add_ps(LOAD_MASK, row_t, row_m);
-      result = _mm512_maskz_add_ps(LOAD_MASK, result, row_b);
+      __m256 result = _mm256_add_ps(row_t, row_m);
+      result = _mm256_add_ps(result, row_b);
 
       // extract rgb
-      __m512i result_int = _mm512_maskz_cvtps_epi32(LOAD_MASK, result);
-      __m128i result_int0 = _mm512_extracti32x4_epi32(result_int, 0);
-      __m128i result_int1 = _mm512_extracti32x4_epi32(result_int, 1);
-      __m128i result_int2 = _mm512_extracti32x4_epi32(result_int, 2);
+      __m256i result_int = _mm256_cvtps_epi32(result);
+      __m128i result_int0 = _mm256_extracti32x4_epi32(result_int, 0);
+      __m128i result_int1 = _mm256_extracti32x4_epi32(result_int, 1);
       int sum_r = _mm_extract_epi32(result_int0, 0) +
                   _mm_extract_epi32(result_int0, 3) +
                   _mm_extract_epi32(result_int1, 2);
@@ -114,7 +110,8 @@ main(int argc, char** argv)
                   _mm_extract_epi32(result_int1, 3);
       int sum_b = _mm_extract_epi32(result_int0, 2) +
                   _mm_extract_epi32(result_int1, 1) +
-                  _mm_extract_epi32(result_int2, 0);
+                  static_cast<char>(cv_r_t_b) + static_cast<char>(cv_r_m_b) +
+                  static_cast<char>(cv_r_b_b);
 
       *(out_base + 0) = static_cast<unsigned char>(sum_r);
       *(out_base + 1) = static_cast<unsigned char>(sum_g);
